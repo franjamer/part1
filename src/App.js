2a65c0841d5dfdc938d2e87b2816414f3fa09274
fjmr <fcojavier.menayo010@gmail.com>
2024-01-05 16:12:44 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
+
+
+const Decription = () => {
+  return <p>Esta es la app del curso fullStack bootcamp</p>
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Titulo de la App</h1>
+    <Mensaje color='red' message='estamos trabajando' />
+    <Mensaje color='blue' message='en un curso'/>
+    <Mensaje color = 'yellow' message='de react'/>
+    <Decription/>
     </div>
-  );
+  )
 }
 
 export default App;
